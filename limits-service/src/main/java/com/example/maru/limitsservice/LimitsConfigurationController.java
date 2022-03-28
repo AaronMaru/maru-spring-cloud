0c1414fcbd0e735a1e71d0766a258917fa363bfa
vichet <vichit.sai@amkcambodia.com>
2022-03-28 10:37:32 +0700

    spring cloud bus done

--- limits-service/src/main/java/com/example/maru/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/example/maru/limitsservice/LimitsConfigurationController.java
@@ -2,6 +2,7 @@ package com.example.maru.limitsservice;
 
 import com.example.maru.limitsservice.bean.LimitConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
---
+++ limits-service/src/main/java/com/example/maru/limitsservice/models/User.java
@@ -0,0 +1,21 @@
+package com.example.maru.limitsservice.models;
+
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+}
